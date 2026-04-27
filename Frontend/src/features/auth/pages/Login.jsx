bbd08d1145e8dfdb28c7f40bbec44ea6790979d2
Ritik Kalal <ritikkalal1234@gmail.com>
2026-04-27 22:59:01 +0530

    Creating Login Page

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import '../auth.form.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <main>
+      <div className='form-container'>
+        <h1>Login</h1>
+        <form>
+          <div className='input-group'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='Enter you email'
+            ></input>
+          </div>
+          <div className='input-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Enter you password'
+            ></input>
+          </div>
+          <button className='button primary-button' type='submit'>
+            Login
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
