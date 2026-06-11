a27193a446a13e3423aa8c3d8203546557b6bd1f
Ritik Kalal <ritikkalal1234@gmail.com>
2026-06-11 17:41:17 +0530

    Created Register form

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
 
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main>
+      <div className='form-container'>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className='input-group'>
+            <label htmlFor='username'>Username</label>
+            <input
+              type='text'
+              id='username'
+              name='username'
+              placeholder='Enter username'
+            ></input>
+          </div>
+          <div className='input-group'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='Enter you email'
+            ></input>
+          </div>
+          <div className='input-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Enter you password'
+            ></input>
+          </div>
+          <button className='button primary-button' type='submit'>
+            Register
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default Register;
