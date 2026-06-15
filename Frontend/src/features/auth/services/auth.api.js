489f4ce001f5606c70bfe34e72953460de57ec03
Ritik Kalal <ritikkalal1234@gmail.com>
2026-06-15 20:39:15 +0530

    auth api for login using axios

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -18,3 +18,21 @@ export async function register({ username, email, password }) {
     console.log(err);
   }
 }
+
+export async function login({ email, password }) {
+  try {
+    const response = await axios.post(
+      'http://localhost:3000/api/auth/login',
+      {
+        email,
+        password,
+      },
+      {
+        withCredentials: true,
+      },
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
